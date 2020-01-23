46d40d1b5033f7090db5d10492291fac0565db7c
jitender jaiswal <jitenderjaiswal686@gmail.com>
2020-01-23 14:24:50 -0500

    Third Commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,37 +1,63 @@
 const fetch = require('node-fetch');
 const jsonData = require('../movies.json');
+const jsonDatas = require('../movie.json');
 
 module.exports.home=function(req, res){
                     return res.render('home');
                    }
+module.exports.movies=async function(req, res){
+                     try{
+                      let id=req.query.id;
+                      if(id=="")
+                      return  res.render('home',{search:jsonData});
+
+                      let url = 'http://www.omdbapi.com/?i=' + id + '&apikey=68fd98ab';
+                      let response=await fetch(url);
+                      let json=await response.json();
+                      let a={"title":json.Title,"description":json.Plot,"duration":json.Runtime,
+                            "Ratings":[{"source":json.Ratings[0].Source,
+                                         "userrating":json.Ratings[0].Value}],
+                            "Cast":[json.Director,json.Writer,json.Actors],"image":json.Poster
+                             };
+                      
+                       let merge;
+                       if(id==jsonData.imdbId)
+                        merge={...jsonData,...a};
+                    
+                     return  res.render('home',{movies:merge});
+                    }catch(err){
+                      console.log('Error', err);return;}  
+ }
 module.exports.search=async function(req,res){
                    try{
                       let ss=[];
                       let s=req.query.s;
-                      let y=req.query.y;
-                      if(s==""&&y=="")
-                      return  res.render('home',{search:jsonData});
+                      if(s=="")
+                      return  res.render('home',{search:jsonDatas});
                     
-                      let url = 'http://www.omdbapi.com/?s=' + s +'&y=' + y + '&apikey=68fd98ab';
+                      let url = 'http://www.omdbapi.com/?s=' + s + '&apikey=68fd98ab';
                       let response=await fetch(url);
                       let json=await response.json();
-                      let str=s.toLowerCase();
-                     
-                      if(json.Response=="False")
-                      {  
-                        for(j of jsonData)
-                        if((str==j.Title.toLowerCase())||(y==j.Year))
-                            ss.push(j);
-                        
-                        return  res.render('home',{search:ss});
-                      }
-                    
-                      for(j of jsonData){ 
-                      if((str==j.Title.toLowerCase())||(y==j.Year))
-                          json.Search.push(j);
-                       }
-                      
+                      let flag;
 
-                     return  res.render('home',{search:json.Search});
+                        for(i of json.Search)
+                       {   flag=false;
+                         for(j of jsonDatas)
+                         {
+                           if(i.Title.toLowerCase()==j.title.toLowerCase())
+                          { let a={"title":i.Title,"productionYear":i.Year,"image":i.Poster,"type":i.Type};
+                            let merge={...j,...a};
+                            ss.push(merge);
+                            flag=true;
+                          }
+                          
+                        }
+                        if(!flag)
+                         { let a={"title":i.Title,"productionYear":i.Year,"image":i.Poster,"type":i.Type};
+                           ss.push(a);
+                         }
+                       }
+                     
+                        return  res.render('home',{search:ss});
                    }catch(err){console.log('Error', err);return;}   
                 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const homeController=require('../controllers/home_controller');
 
 router.get('/',homeController.home);
 router.get('/search',homeController.search);
+router.get('/movies/',homeController.movies);
 
 module.exports = router;
